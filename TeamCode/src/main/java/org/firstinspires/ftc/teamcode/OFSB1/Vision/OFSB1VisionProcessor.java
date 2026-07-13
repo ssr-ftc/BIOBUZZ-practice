970fd36a80c9b3990b427732812c639a710a24a9
akash <Akashvijayaradhya@gmail.com>
2026-07-13 12:53:05 -0700

    Add EasyOpenCV dependency and initial Vision processor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OFSB1/Vision/OFSB1VisionProcessor.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.OFSB1.Vision;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+/**
+ * Basic EasyOpenCV pipeline for OFSB1.
+ */
+public class OFSB1VisionProcessor extends OpenCvPipeline {
+
+    @Override
+    public Mat processFrame(Mat input) {
+        // Add your vision processing logic here
+        return input;
+    }
+}
